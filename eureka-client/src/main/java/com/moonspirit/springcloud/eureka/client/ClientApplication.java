52b4bc78d825af6981e3258ad83af5459d74e1bd
moonspiritacm <yefanbupt@gmail.com>
2019-08-12 17:28:54 +0800

    init

--- eureka-client/src/main/java/com/moonspirit/springcloud/eureka/client/ClientApplication.java
+++ eureka-client/src/main/java/com/moonspirit/springcloud/eureka/client/ClientApplication.java
@@ -2,9 +2,9 @@ package com.moonspirit.springcloud.eureka.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class ClientApplication {
 
